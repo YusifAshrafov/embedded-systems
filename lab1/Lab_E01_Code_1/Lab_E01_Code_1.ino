d78ba0cad39b5cdab1dec855bfb760b0230d5f5a
Yusif Ashrafov <130373670+YusifAshrafov@users.noreply.github.com>
2026-03-19 21:24:43 +0400

    Update Lab_E01_Code_1.ino

--- lab1/Lab_E01_Code_1/Lab_E01_Code_1.ino
+++ lab1/Lab_E01_Code_1/Lab_E01_Code_1.ino
@@ -1,23 +1,25 @@
-int led1 = 8;
-int led2 = 9;
-int led3 = 10;
+int led1 = 8;                 // create a variable LED1 and assign pin 8
+int led2 = 9;                 // create a variable LED2 and assign pin 9
+int led3 = 10;                // create a variable LED3 and assign pin 10
 
-void setup() {
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+void setup() {                // runs once when the board starts
+  pinMode(led1, OUTPUT);      // set pin8 as output to control LED1
+  pinMode(led2, OUTPUT);      // set pin9 as output to control LED2
+  pinMode(led3, OUTPUT);      // set pin10 as output to control LED3
 }
    
-void loop() {
-  digitalWrite(led1, HIGH);
-  delay(500);
-  digitalWrite(led1, LOW);
+void loop() {                 // runs repeatedly
+  digitalWrite(led1, HIGH);   // LED1 turn ON by sending HIGH voltage to pin8
+  delay(500);                 // wait for 500 millis = 5 sec
+  digitalWrite(led1, LOW);    // LED1 turn OFF by sending LOW voltage to pin8
 
-  digitalWrite(led2, HIGH);
-  delay(500);
-  digitalWrite(led2, LOW);
+  digitalWrite(led2, HIGH);   // LED2 turn ON by sending HIGH voltage to pin9
+  delay(500);                 // wait for 500 millis = 5 sec
+  digitalWrite(led2, LOW);    // LED2 turn OFF by sending LOW voltage to pin9
 
-  digitalWrite(led3, HIGH);
-  delay(500);
-  digitalWrite(led3, LOW);
+  digitalWrite(led3, HIGH);   // LED3 turn ON by sending HIGH voltage to pin10
+  delay(500);                 // wait for 500 millis = 5 sec
+  digitalWrite(led3, LOW);    // LED3 turn OFF by sending LOW voltage to pin10
 }
+
+// delay(500); - comment this out to test flashing as quickly as possible
